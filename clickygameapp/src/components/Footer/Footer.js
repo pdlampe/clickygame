4bcb5ee0a77021cb84ed1dd192b640feee3d41af
Paul David Lampe <pdlampe@gmail.com>
2019-10-13 16:02:29 -0500

    Updated footer content

---
+++ clickygameapp/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <footer className="footer text-center">
+        <span>Clicky Game&nbsp;|&nbsp;<a href="https://github.com/pdlampe" target="_blank">GitHub Repo: Paul David Lampe</a></span>
+    </footer>
+);
+
+export default Footer;
