66b00d8f39fe35fa9862c9f947553df2d0437767
Paul David Lampe <pdlampe@gmail.com>
2019-10-17 20:39:22 -0500

    Updates

---
+++ precache-manifest.855d49d1eea8ebe0c920cb1073b9c76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48a79e43d09ee3dffe85c1f6b707f34",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "1f0e715f5f6bdb536b01",
+    "url": "/clickygame/static/css/main.67d940f7.chunk.css"
+  },
+  {
+    "revision": "a9093b12bdf9569daa9b",
+    "url": "/clickygame/static/js/2.8f809ca8.chunk.js"
+  },
+  {
+    "revision": "1f0e715f5f6bdb536b01",
+    "url": "/clickygame/static/js/main.a0d14305.chunk.js"
+  },
+  {
+    "revision": "190b191712883583562b",
+    "url": "/clickygame/static/js/runtime-main.35921983.js"
+  },
+  {
+    "revision": "af3acf46bd13e4d7ac4e9445a3ee0e3d",
+    "url": "/clickygame/static/media/binoculars.af3acf46.jpg"
+  }
+]);
